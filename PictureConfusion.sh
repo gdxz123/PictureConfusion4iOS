f4b8221ef47a6a893461acf88f728fa612c84c14
zhangqingwang <815570979@qq.com>
2018-10-20 00:28:24 +0800

    Change: 1、增加参数输入 2、生成文件在新的文件夹

--- PictureConfusion.sh
+++ PictureConfusion.sh
@@ -3,6 +3,21 @@ imageNumber=0
 files='*'
 fileArray=($files)
 # echo total number : ${#fileArray[@]}
+preString="pre_"
+newPath="confuseResult"
+
+originPath=$1
+
+if [ ! -n "$originPath" ];then
+	echo "\033[31m\033[01myou must input picture resource path!!!\033[0m"
+	exit
+else
+	echo "the picture resource path is $originPath"
+fi
+
+cd ..
+mkdir $newPath
+cd $originPath
 
 for fileName in ${fileArray[@]}
 do
@@ -10,7 +25,7 @@ do
 	if [ "${fileName##*.}" = "png" ] || [ "${fileName##*.}" = "PNG" ]  || [ "${fileName##*.}" = "jpg" ] || [ "${fileName##*.}" = "JPG" ] 
 	then
 		imageNumber=$[$imageNumber+1];
-		cat $fileName > "pre_$fileName"
+		cat $fileName > "../$newPath/$preString$fileName"
 	fi
 
 	if [ "${fileName##*.}" = "json" ]
